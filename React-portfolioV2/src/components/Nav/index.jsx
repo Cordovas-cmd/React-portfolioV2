f957bc31d6b41066b5bb68f5c1517aebc78c033d
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-08-30 19:58:38 -0500

    okay, now I'm serious, that pesky nav styling is all good :D

--- React-portfolioV2/src/components/Nav/index.jsx
+++ React-portfolioV2/src/components/Nav/index.jsx
@@ -7,20 +7,21 @@ function Nav({ currentPage }) {
   return (
     <nav className="navbar navbar-expand-lg ">
     <div className="container-fluid">
+    <a className="navbar-brand" href="#">Samuel Cordova</a>
       <ul className="flex-row navbar-nav ml-auto">
         <li
-          className={` nav-item   ${currentPage === '/' && 'navActive'}`}
+          className={` nav-item  ${currentPage === '/' && 'navActive'}`}
         key="about"
         >
         {/* <a className=
         {` nav-link mx-5 ${currentPage === '/' && 'navActive'}`}
         key="about"> */}
-          <Link className="nav-link " to="/">About</Link>
+          <Link className="nav-link" to="/">About</Link>
        {/* </a> */}
         </li>
         {pages.map((Page) => (
           <li
-              className={` nav-item  ${currentPage === '/' && 'navActive'}`}
+              className={` nav-item  ${currentPage === `/${Page}` && 'navActive'}`}
               key={Page}
            
           >
